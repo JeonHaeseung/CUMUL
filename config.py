0779fa4554ce02909e747a48ab8932a9bae5493f
JeonHaeseung <414catherine@gmail.com>
2024-12-12 12:11:11 +0900

    [rename] Reformatted the codebase structure

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-background_data = '/home/liyanzeng/download/foreground-data/output-tcp/'
-foreground_data = '/home/liyanzeng/download/foreground-data/output-tcp/'
-temp_path = os.path.join(os.path.abspath('./'), './temp/')
--- main.py
+++
@@ -1,9 +0,0 @@
-from utils.data import *
-from config import *
-
-def main():
-    preprogress(foreground_data)
-
-
-if __name__ == '__main__':
-    main()
